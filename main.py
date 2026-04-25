452bb930f32b4cf70bd48c10873ba95e61d02688
reaperlxxsdev <wb2m526gkb@privaterelay.appleid.com>
2026-04-24 20:16:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import asyncio
 import cv2
 import base64
 import os
+import traceback
 import gdown
 import gymnasium as gym
 import ale_py.roms
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from stable_baselines3 import DQN
 
-# --- NUEVA SECCIÓN: DESCARGA AUTOMÁTICA DEL MODELO ---
+# --- SECCIÓN: DESCARGA AUTOMÁTICA DEL MODELO ---
 file_id = '1KeJDGDuFoovIOK-qXYY3vjGW-78CKMGP'
 url = f'https://drive.google.com/uc?id={file_id}'
 output_file = 'ddqn_mspacman_500k_pasos.zip'
 
-# Solo lo descarga si el archivo no existe en el servidor
 if not os.path.exists(output_file):
     print("Descargando el cerebro de la IA desde Google Drive...")
     gdown.download(url, output_file, quiet=False)
@@ -35,7 +35,7 @@ async def play_game(websocket: WebSocket):
     try:
         # Bucle infinito del juego
         while True:
-            # La IA decide el movimiento (sin exploración aleatoria)
+            # La IA decide el movimiento
             accion, _ = modelo.predict(obs, deterministic=True)
 
             # Ejecutamos el movimiento en el emulador
@@ -44,25 +44,30 @@ async def play_game(websocket: WebSocket):
             # Capturamos la pantalla actual
             frame = env.render()
 
-            # OpenCV usa BGR en lugar de RGB, invertimos los colores para que no se vea azul
+            # OpenCV usa BGR en lugar de RGB
             frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-            # Comprimimos la imagen a JPEG para que viaje rápido por internet
+            # Comprimimos la imagen a JPEG
             _, buffer = cv2.imencode('.jpg', frame_bgr)
 
-            # La convertimos a texto (Base64) para poder enviarla por el WebSocket
+            # Convertimos a texto (Base64)
             frame_b64 = base64.b64encode(buffer).decode('utf-8')
 
             # Enviamos el fotograma al Frontend
             await websocket.send_text(frame_b64)
 
-            # Si Pac-Man muere o se acaba el tiempo, reiniciamos la partida automáticamente
+            # Reinicio de partida
             if done or truncado:
                 obs, info = env.reset()
 
-            # Agregamos una pequeñísima pausa (aprox 30 FPS) para no saturar el navegador
+            # Pausa de 30 FPS
             await asyncio.sleep(0.03)
 
+    except WebSocketDisconnect:
+        # Esto solo se activa si tú cierras la pestaña de Netlify
+        print("El cliente cerró la pestaña (Desconexión normal).")
+        
     except Exception as e:
-        # Si el usuario cierra la pestaña, terminamos la conexión limpiamente
-        print("El cliente se ha desconectado.")
+        # Esto atrapará el error de la Red Neuronal e imprimirá qué le duele
+        print(f"🔥 ALERTA ROJA: El juego colapsó con este error: {e}")
+        traceback.print_exc()
